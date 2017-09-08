50a5ca0e2214ef7c463348c643ce33d775247a97
wpatter6 <wpatter6@gmail.com>
2017-09-08 11:50:26 -0600

    Adding file logging for errors

--- PokemonGoRaidBot/Services/ConsoleLogger.cs
+++ PokemonGoRaidBot/Services/ConsoleLogger.cs
@@ -1,6 +1,7 @@
 ﻿using Discord;
 using System;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace PokemonGoRaidBot.Services
 {
@@ -30,10 +31,29 @@ namespace PokemonGoRaidBot.Services
                     Console.ForegroundColor = ConsoleColor.DarkGray;
                     break;
             }
+
+            var error = lmsg.Exception;
+
+            if(error != null)
+            {
+                LogToFile(error.ToString());
+            }
+
             var message = lmsg.Exception != null ? lmsg.Exception.Message : lmsg.Message;
             Console.WriteLine($"{DateTime.Now} [{lmsg.Severity,8}] {lmsg.Source}: {message}");
             Console.ForegroundColor = cc;
             return Task.CompletedTask;
+        }
+
+        public void LogToFile(string message, string folder = "log")
+        {
+            var fileName = string.Format("/log_{0}", DateTime.Now.Date.ToShortDateString());
+            var path = Path.Combine(AppContext.BaseDirectory, folder);
+            var loc = Path.Combine(path, fileName);
+            var msg = string.Format("{0}: {1}", DateTime.Now.ToShortTimeString(), message);
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+
+            File.AppendAllLines(path, new string[] { msg });
         }
     }
 }
