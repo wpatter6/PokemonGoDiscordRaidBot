2f105890a47a152cab722a4da4b38bec4be3a613
Will Patterson <wpatter6@gmail.com>
2017-09-22 23:38:52 -0600

    Modifying publish order

--- publish.ps1
+++ publish.ps1
@@ -5,21 +5,26 @@ new-item -Name Releases -ItemType directory
 
 dotnet restore
 
-dotnet publish -c release -r win10-x64
-dotnet publish -c release -r win8-x64
-dotnet publish -c release -r win7-x64
-dotnet publish -c release -r ubuntu.16.10-x64
-dotnet publish -c release -r osx.10.11-x64
-
 Add-Type -A System.IO.Compression.FileSystem
-[IO.Compression.ZipFile]::CreateFromDirectory('PokemonGoRaidBot\bin\Release\netcoreapp2.0\osx.10.11-x64\publish', 'Releases\PokemonGoDiscordRaidBot_osx.10.11-x64.zip')
 
-[IO.Compression.ZipFile]::CreateFromDirectory('PokemonGoRaidBot\bin\Release\netcoreapp2.0\ubuntu.16.10-x64\publish', 'Releases\PokemonGoDiscordRaidBot_ubuntu.16.10-x64.zip')
+dotnet publish -c release -r win10-x64
 
 [IO.Compression.ZipFile]::CreateFromDirectory('PokemonGoRaidBot\bin\Release\netcoreapp2.0\win10-x64\publish', 'Releases\PokemonGoDiscordRaidBot_win10-x64.zip')
 
+dotnet publish -c release -r win8-x64
+
 [IO.Compression.ZipFile]::CreateFromDirectory('PokemonGoRaidBot\bin\Release\netcoreapp2.0\win8-x64\publish', 'Releases\PokemonGoDiscordRaidBot_win8-x64.zip')
 
+dotnet publish -c release -r win7-x64
+
 [IO.Compression.ZipFile]::CreateFromDirectory('PokemonGoRaidBot\bin\Release\netcoreapp2.0\win7-x64\publish', 'Releases\PokemonGoDiscordRaidBot_win7-x64.zip')
+
+dotnet publish -c release -r ubuntu.16.10-x64
+
+[IO.Compression.ZipFile]::CreateFromDirectory('PokemonGoRaidBot\bin\Release\netcoreapp2.0\ubuntu.16.10-x64\publish', 'Releases\PokemonGoDiscordRaidBot_ubuntu.16.10-x64.zip')
+
+dotnet publish -c release -r osx.10.11-x64
+
+[IO.Compression.ZipFile]::CreateFromDirectory('PokemonGoRaidBot\bin\Release\netcoreapp2.0\osx.10.11-x64\publish', 'Releases\PokemonGoDiscordRaidBot_osx.10.11-x64.zip')
 
 pause
